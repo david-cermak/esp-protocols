fe87fd0aa60c3a1e7698bcd405b5b699ffbd447c
David Cermak <cermak@espressif.com>
2024-06-27 09:48:25 +0200

    fix(mosq): Add ESP-2.0 licenses

--- components/mosq/port/priv_include/config.h
+++ components/mosq/port/priv_include/config.h
@@ -10,12 +10,15 @@
 #define isspace(__c) (__ctype_lookup((int)__c)&_S)
 
 #include_next "config.h"
-
 #define VERSION "0.1.0"
 
-//#define sysconf(x) (64)
+#define _SC_OPEN_MAX_OVERRIDE                 4
 
+// used to override syscall for obtaining max open fds
 static inline long sysconf(int arg)
 {
-    return 64;
+    if (arg == _SC_OPEN_MAX_OVERRIDE) {
+        return 64;
+    }
+    return -1;
 }
